07b1657ba534082d40dff355b1cc3168a9e5753f
hyunbell <man.of.backend@gmail.com>
2022-10-07 20:35:26 +0900

    chore: 구글 로그인

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
-import { useSession } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { dehydrate, QueryClient } from "react-query";
 import InputElement from "../components/InputElement";
 import NameLengthElement from "../components/NameLengthElement";
@@ -9,6 +10,7 @@ import { fetchPosts } from "../hooks/usePosts";
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -26,12 +28,20 @@ const Home: NextPage = () => {
           </h1>
         ) : (
           <>
-            <InputElement />
-            <NameLengthElement />
+            <h1 className="pb-8 text-6xl font-bold">로그인 해주세요!</h1>
+            <div className="flex flex-col justify-center gap-5">
+              <InputElement />
+              <NameLengthElement />
+              <button
+                className="h-8 animate-bounce shadow-md"
+                onClick={() => signIn()}
+              >
+                로그인하기
+              </button>
+            </div>
           </>
         )}
       </main>
-
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
