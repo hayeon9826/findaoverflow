2f2846088038d59435f4867f4ce2f056d256ac59
hyunbell <man.of.backend@gmail.com>
2022-10-30 23:56:42 +0900

    feat: fetchPostsFromFirebase

--- apis/posts.ts
+++ apis/posts.ts
@@ -1,6 +1,43 @@
 import { getApiAddress } from 'config/api';
+import { getFirebaseAdmin } from 'config/firebaseAdmin';
 import { PostType } from 'config/interface';
+import { apiInstance } from 'utils/axios';
 
-export function getPosts(): Promise<{ posts: PostType[] }> {
-  return fetch(getApiAddress('POSTS')).then((res) => res.json());
+const DEFAULT_POSTS_FETCH_COUNT = 5;
+interface Props {
+  count?: number;
+  ts?: number;
+}
+export function getPosts({ count = 10, ts }: Props = {}): Promise<{
+  posts: PostType[];
+}> {
+  return apiInstance
+    .get(getApiAddress('POSTS'), { params: { count, ts } })
+    .then((res) => res.data);
+}
+
+export async function fetchPostsFromFirebase({
+  count = DEFAULT_POSTS_FETCH_COUNT,
+  ts,
+}: Props = {}): Promise<PostType[]> {
+  const admin = getFirebaseAdmin();
+
+  const timestamp = ts
+    ? admin.firestore.Timestamp.fromMillis(Number(ts) * 1000)
+    : admin.firestore.Timestamp.fromDate(new Date(2099, 12, 31));
+
+  const data = await admin
+    .firestore()
+    .collection('posts')
+    .orderBy('createdAt', 'desc')
+    .startAfter(timestamp)
+    .limit(count)
+    .get();
+
+  const posts = data.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  })) as PostType[];
+
+  return posts;
 }
